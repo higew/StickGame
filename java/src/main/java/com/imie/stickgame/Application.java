053b5f42fe8e628036b9f1527b5caaeca04b34c8
Romain Matteo <romain.matteo@imie.fr>
2018-11-09 10:00:54 +0100

    Starting Java

---
+++ java/src/main/java/com/imie/stickgame/Application.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+
+package com.imie.stickgame;
+
+/**
+ * @author Romain
+ *
+ */
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
