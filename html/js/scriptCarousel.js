aa44df61a2f89dd565ecf16cf0ca15f31cd1a33e
Andrin Camille <camille.andrin@gmail.com>
2018-12-05 16:55:36 +0100

    du css, du js, et pis c'est tout !

--- html/js/scriptCarousel.js
+++ html/js/scriptCarousel.js
@@ -622,12 +622,12 @@
   $.fn.waterwheelCarousel.defaults = {
     // number tweeks to change apperance
     startingItem:               1,   // item to place in the center of the carousel. Set to 0 for auto
-    separation:                 320, // distance between items in carousel
+    separation:                 250, // distance between items in carousel
     separationMultiplier:       1, // multipled by separation distance to increase/decrease distance for each additional item
     horizonOffset:              0,   // offset each item from the "horizon" by this amount (causes arching)
     horizonOffsetMultiplier:    0,   // multipled by horizon offset to increase/decrease offset for each additional item
     sizeMultiplier:             1, // determines how drastically the size of each item changes
-    opacityMultiplier:          1, // determines how drastically the opacity of each item changes
+    opacityMultiplier:          5, // determines how drastically the opacity of each item changes
     horizon:                    0,   // how "far in" the horizontal/vertical horizon should be set from the container wall. 0 for auto
     flankingItems:              10,   // the number of items visible on either side of the center
 
@@ -635,7 +635,7 @@
     speed:                      300,      // speed in milliseconds it will take to rotate from one to the next
     animationEasing:            'linear', // the easing effect to use when animating
     quickerForFurther:          false,     // set to true to make animations faster when clicking an item that is far away from the center
-    edgeFadeEnabled:           false,    // when true, items fade off into nothingness when reaching the edge. false to have them move behind the center image
+    edgeFadeEnabled:           true,    // when true, items fade off into nothingness when reaching the edge. false to have them move behind the center image
 
     // misc
     linkHandling:               2,                 // 1 to disable all (used for facebox), 2 to disable all but center (to link images out)
@@ -648,8 +648,8 @@
 
     // preloader
     preloadImages:              true,  // disable/enable the image preloader.
-    forcedImageWidth:           300,     // specify width of all images; otherwise the carousel tries to calculate it
-    forcedImageHeight:          450,     // specify height of all images; otherwise the carousel tries to calculate it
+    forcedImageWidth:           200,     // specify width of all images; otherwise the carousel tries to calculate it
+    forcedImageHeight:          300,     // specify height of all images; otherwise the carousel tries to calculate it
 
     // callback functions
     movingToCenter:             $.noop, // fired when an item is about to move to the center position
